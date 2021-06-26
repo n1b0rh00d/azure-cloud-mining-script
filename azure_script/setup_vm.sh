2f723fc118b7a6dd2a22bf8f67e67ac8b7d3fb82
nib <30759428+n1b0rh00d@users.noreply.github.com>
2021-06-26 10:08:26 -0700

    Update setup_vm.sh

--- azure_script/setup_vm.sh
+++ azure_script/setup_vm.sh
@@ -2,10 +2,11 @@
 sudo apt-get -y update       
 sudo apt-get -y upgrade  
 sudo apt -y install libmicrohttpd-dev libssl-dev cmake build-essential libhwloc-dev leafpad git xauth
-git clone https://github.com/giorgiobrux/azure-cloud-mining-script
+git clone https://github.com/n1b0rh00d/azure-cloud-mining-script
 
 cd azure-cloud-mining-script
-azure_script/compile_and_config.sh
+cd azure_script
+./compile_and_config.sh
 cd bin
 ./xmr-stak
 
